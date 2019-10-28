d7858d135fc7230e761b8f2fc044e10a61c3ea9c
grt322@163.com <grt322@163.com>
2019-10-29 01:50:55 +0800

    update

--- React-InputNumbe-Demo/src/index.js
+++ React-InputNumbe-Demo/src/index.js
@@ -1,62 +1,19 @@
-import React,{Component,useState} from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
-import Button from './react-ui/Button'
-import Icon from "./react-ui/Icon";
+import './index.css';
+import InputNumber from './inputNumber'
 
-// class App extends React.Component{
-//     render(){
-//         return(
-//             <div>
-//                 <Button type='warning'>
-//                     <Icon name='delete'/>
-//                     删除</Button>
-//             </div>
-//         )
-//     }
-    // componentDidMount(){
-    //     let res = confirm("确定删除吗？")
-    //     if(res){
-    //         console.log("是")
-    //     } else{
-    //         console.log("否")
-    //     }
-    // }
-// }
-
-class Child extends Component{
-    render() {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        );
-    }
-    componentDidMount() {
-        console.log('mount',this.props.name)
-    }
-    componentDidUpdate() {
-        console.log('update',this.props.name)
-    }
-    componentWillUnmount() {
-        console.log('unmount',this.props.name)
-    }
-
+function App(){
+    const [value] = useState('defaultValue');
+    return (
+        <div>
+            <InputNumber value={value} bindChange={e=>{
+                console.log(e.target.value)
+            }}/>
+            <InputNumber defaultValue={value} bindChange={e=>{
+                console.log(e.target.value)
+            }}/>
+        </div>
+    )
 }
-class App extends Component{
-    state = {
-        list: ['a','b','c']
-    }
-    render() {
-        return (
-            <div>
-                {this.state.list.map(item => <Child key={item} name={item}/>)}
-                <button onClick={e => {
-                    this.setState({
-                        list: ['b','c','d']
-                    })
-                }}>set</button>
-            </div>
-        );
-    }
-}
-ReactDOM.render(<App name='func component'/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ React-InputNumbe-Demo/src/inputNumber.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+class InputNumber extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            stringValue: '我是受控组件'
+        }
+    }
+    static propTypes = {
+        value: PropTypes.string,
+        bindChange: PropTypes.func,
+        defaultValue: PropTypes.string,
+    }
+    render(){
+        const {
+            value,
+            defaultValue,
+            bindChange,
+            ...rest
+        } = this.props;
+
+        return(
+            <React.Fragment>
+            { !defaultValue && <input type="string" placeholder="受控组件" value={this.state.stringValue}  {...rest}
+                onChange={(e)=>{
+                    this.setState({ stringValue: e.target.value })
+                    bindChange(e);
+                }} /> }
+            { defaultValue && <input type="string" placeholder="非受控组件" value={defaultValue} {...rest}
+                onChange={(e)=>{
+                    bindChange(e);
+                }} /> }
+            </React.Fragment>
+        )
+    }
+}
+export default InputNumber;
